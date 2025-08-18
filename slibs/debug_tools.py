5f9d71afddae790de322a309840714c09ccf13c1
sgommitos <dariosommanerd@gmail.com>
2025-08-18 12:55:16 +0200

    BUGFIX: fix word distance from arrow in decorators' debug functions

--- slibs/debug_tools.py
+++ slibs/debug_tools.py
@@ -5,7 +5,7 @@ from slibs.printl import *
 def not_implemented(func):
     @functools.wraps(func)
     def wrapper(*args, **kwargs):
-        print(bg_text(f"Function '{func.__name__}' ⟹ NOT YET IMPLEMENTED", RED))
+        print(bg_text(f"Function '{func.__name__}' ⟹   NOT YET IMPLEMENTED", RED))
     
     return wrapper
 
@@ -13,7 +13,7 @@ def not_fully_implemented():
     def decorator(func):
         @functools.wraps(func)
         def wrapper(*args, **kwargs):
-            print(bg_text(f"Function '{func.__name__}' ⟹ NOT FULLY IMPLEMENTED", CYAN))
+            print(bg_text(f"Function '{func.__name__}' ⟹   NOT FULLY IMPLEMENTED", CYAN))
             
             result = func(*args, **kwargs)
             return result
